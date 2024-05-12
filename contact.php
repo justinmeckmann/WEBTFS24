8d180c8d9585241e60b52304a032c8a2ca16c1eb
Justin Meckmann <justin.meckmann@stud.hslu.ch>
2024-05-12 09:54:16 +0200

    added files

---
+++ contact.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Confirmation</title>
+        <meta charset="utf8">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    </head>
+
